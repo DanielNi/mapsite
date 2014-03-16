0827fdec8d910eb56b16279bb88a1de72289abc3
Daniel Ni <danielni@wustl.edu>
2014-03-16 02:58:45 -0500

    profiles updated

--- mapsite/urls.py
+++ mapsite/urls.py
@@ -1,26 +1,37 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib.auth import views
+from django.contrib.auth.decorators import user_passes_test
+
 from django.contrib import admin
 admin.autodiscover()
 
+login_forbidden = user_passes_test(lambda u: u.is_anonymous(), 'profile')
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mapsite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^home/$', login_forbidden(views.login), name="login"),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'myapp.views.index', name="homepage"),
     url(r'^home/$', 'myapp.views.index', name="home"),
     url(r'^about/$', 'myapp.views.about', name="about"),
 
+    url(r'^login/$', views.login,
+                        {'template_name': 'registration/login.html'},
+                           name="auth_login"),
+    url(r'^logout/$', views.logout,
+                        {'next_page': 'homepage'},
+                        name="auth_logout"),
+
  #    url(r'^login/$', 'django.contrib.auth.views.login', {
  #    	'template_name': 'login.html'}, name="login"),
 	# url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
  #    	name="logout"),
-
     url(r'^', include('registration.backends.simple.urls')),
-    url(r'^', include('registration.auth_urls')),
+    # url(r'^', include('registration.auth_urls')),
 
-    url(r'^users/profile', 'registration.views.profile'),
+    url(r'^users/profile', 'registration.views.profile', name="profile"),
     url(r'^users/', 'profiles.views.profile'),
+
+
 )
