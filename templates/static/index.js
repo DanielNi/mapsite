8075b172df30c765c92be7db52421c6b3cd1ede4
Daniel Ni <danielni@wustl.edu>
2014-07-30 23:56:25 -0500

    updated profiles

--- templates/static/index.js
+++ templates/static/index.js
@@ -1,70 +1,103 @@
-$(function(){
-	$('#header_nav').data('size','big');
-});
+var index = (function($) {
 
-$(window).scroll(function(){
-	if ($(document).scrollTop() > 75)
-	{
-		if ($('#header_nav').data('size') === 'big')
-		{
-			$('#header_nav').data('size','small');
-			$('#header_nav').stop().animate({
-				height:'50px'
-			},600);
-			$('.navbar-brand').stop().animate({
-				fontSize:'20px',
-				marginTop:'0px'
-			},600);
-			$('#menu').stop().animate({
-				fontSize:'14px',
-				marginTop:'0px'
-			},600);
+	var elements = {
+		acquire:function() {
+			this.window 		= $(window);
+			this.document		= $(document);
+			this.header_nav		= $('#header_nav');
+			this.navbar_brand	= $('.navbar-brand');
+			this.menu			= $('#menu');
+			this.overlay		= $('.overlay');
+			this.login 			= $('.login');
+			this.login_frame	= $('#login_frame');
+			this.btn_primary	= $('.btn-primary');
+			this.register_frame = $('#register_frame');	
 		}
-	}
-	else
-	{
-		if ($('#header_nav').data('size') === 'small')
-		{
-			$('#header_nav').data('size','big');
-			$('#header_nav').stop().animate({
-				height:'80px'
-			},600);
-			$('.navbar-brand').stop().animate({
-				fontSize:'35px',
-				marginTop:'15px'
-			},600);
-			$('#menu').stop().animate({
-				fontSize:'20px',
-				marginTop:'15px'
-			},600);
-		}  
-	}
+	};
+
+	var ev = {
+		bind:function() {
+			elements.window.scroll(ev.change_header_size);
+			elements.login.on('click', ev.show_login);
+			elements.btn_primary.on('click', ev.show_register);
+			elements.overlay.on('click', ev.close_everything);
+		},
+
+		change_header_size:function() {
+			if (elements.document.scrollTop() > 75)
+			{
+				if (elements.header_nav.data('size') === 'big')
+				{
+					elements.header_nav.data('size','small');
+					elements.header_nav.stop().animate({
+						height:'50px'
+					},600);
+					elements.navbar_brand.stop().animate({
+						fontSize:'20px',
+						marginTop:'0px'
+					},600);
+					elements.menu.stop().animate({
+						fontSize:'14px',
+						marginTop:'0px'
+					},600);
+				}
+			}
+			else
+			{
+				if (elements.header_nav.data('size') === 'small')
+				{
+					elements.header_nav.data('size','big');
+					elements.header_nav.stop().animate({
+						height:'80px'
+					},600);
+					elements.navbar_brand.stop().animate({
+						fontSize:'35px',
+						marginTop:'15px'
+					},600);
+					elements.menu.stop().animate({
+						fontSize:'20px',
+						marginTop:'15px'
+					},600);
+				}  
+			}
+		},
+
+		show_login:function() {
+			if (!elements.login_frame.is(':visible')) {
+				elements.login_frame.fadeIn();
+				elements.overlay.fadeIn();
+			}
+		},
+
+		show_register:function() {
+			if (!elements.register_frame.is(':visible')) {
+				elements.register_frame.fadeIn();
+				elements.overlay.fadeIn();
+			}
+		},
+
+		close_everything:function() {
+			if (elements.login_frame.is(":visible")) {
+				elements.login_frame.fadeOut();
+				elements.overlay.fadeOut();
+			}
+			if (elements.register_frame.is(':visible')) {
+				elements.register_frame.fadeOut();
+				elements.overlay.fadeOut();
+			}
+		}
+	};
+
+	elements.acquire();
+
+	return {
+		init:function() {
+			ev.bind();
+		}
+	};
+
+})(jQuery);
+
+jQuery(function($) {
+	index.init();
 });
-
-var showing_login = false;
-var showing_register = false;
-
-$('.overlay').on('click', function() {
-	if ($('#login_frame').is(":visible")) {
-		$('#login_frame').fadeOut();
-		$('.overlay').fadeOut();
-	}
-	if ($('#register_frame').is(':visible')) {
-		$('#register_frame').fadeOut();
-		$('.overlay').fadeOut();
-	}
-});
-
-$('.login').on('click', function() {
-	if (!$('#login_frame').is(':visible')) {
-		$('#login_frame').fadeIn();
-		$('.overlay').fadeIn();
-	}
-});
-
-$('.btn-primary').on('click', function() {
-	if (!$('#register_frame').is(':visible')) {
-		$('#register_frame').fadeIn();
-		$('.overlay').fadeIn();
-	}
-})
