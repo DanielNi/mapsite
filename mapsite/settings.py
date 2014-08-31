5be945aad467d6730206d4d9f96df6bedc677e9a
Daniel Ni <danielni@wustl.edu>
2014-08-31 13:35:34 -0500

    fixed static files settings?

--- mapsite/settings.py
+++ mapsite/settings.py
@@ -100,8 +100,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    'templates/static/',
+    os.path.join(BASE_DIR, "templates/static")
 )
 
 LOGIN_URL=reverse_lazy('login')
