2605d557046611a2f6272d30d36e354a56bc0494
Daniel Ni <danielni@wustl.edu>
2014-11-03 22:10:37 -0600

    removed some stuff

--- myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def index(request):
-    return render_to_response('index.html')
-
-def example(request):
-	return render_to_response('example.html')
